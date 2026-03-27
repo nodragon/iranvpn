85e1175d90ba4eee1d6e4588f99dd391db9344af
nodragon <57659981+nodragon@users.noreply.github.com>
2026-03-27 18:19:36 +0800

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,18 +1,31 @@
+
 pluginManagement {
     repositories {
+        gradlePluginPortal()
         google()
         mavenCentral()
-        gradlePluginPortal()
+    }
+
+    plugins {
+        id("com.android.application") version "8.7.2"
+        id("com.android.library") version "8.7.2"
+        id("org.jetbrains.kotlin.android") version "2.0.21"
+        id("org.jetbrains.kotlin.plugin.compose") version "2.0.21"
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        // Psiphon (optional): add dependency implementation("ca.psiphon:psiphontunnel:2.0.35")
-        maven { url = uri("https://raw.githubusercontent.com/Psiphon-Labs/psiphon-tunnel-core-Android-library/master") }
+
+        // Psiphon (optional)
+        maven {
+            url = uri("https://raw.githubusercontent.com/Psiphon-Labs/psiphon-tunnel-core-Android-library/master")
+        }
     }
 }
+
 rootProject.name = "IranVPN"
 include(":app")
